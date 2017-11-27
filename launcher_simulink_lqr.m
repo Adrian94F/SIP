4c988babcc384ea56eb5e59c5e316ca794c508bc
Dave94 <dave944@interia.pl>
2017-11-27 21:33:44 +0100

    Wygenerowane dane do NL

--- launcher_simulink_lqr.m
+++ launcher_simulink_lqr.m
@@ -17,9 +17,10 @@ theta0 = 1/20 * pi;
 xDot0 = 0;
 x0 = 0;
 state0 = [thetaDot0;theta0;xDot0;x0]; % wektor stanu poczatkowego wahadla
-nOfGenParams = 5;
-randomParams = [rand(nOfGenParams, 4)];
-results = [];
+nOfGenParams = 100;
+randomParams = rand(nOfGenParams, 4)*10;
+lqr_params = zeros(nOfGenParams, 4);
+tic();
 for i=1:nOfGenParams
     % M = 0.5; % masa wozka
     % m = 0.2; % masa wahadla
@@ -31,25 +32,16 @@ for i=1:nOfGenParams
     m = randomParams(i,2);
     L = randomParams(i,3);
     I = randomParams(i,4);
+    
     params = [M,m,L,I,b,g]; % wektor parametrow wahadla do s-funkcji
+    
     % wybor modelu i regulatora
     modelSelect = 'linear'; % wybor rodzaju modelu: full / linear
 
     % parametry regulatorow
     % LQR
     K = strojenie_lqr(M,m,L,b);
-    %K = [-6.6845,-51.6047,-9.6527,-10];
     N = K(4);
-
-    % symulacja z simulinka
-    disp('Start symulacji...');
-    tic();
-    % wybor modelu do symulacji
-    sim('stabilizacja_lqr.slx');
-    disp('Koniec symulacji');
-    toc();
-    results = [results isStabilised(tSim, h, y)];
-    % wykresy
-    % run plots;
-    % run anims;
+    lqr_params(i,:) = strojenie_lqr(M,m,L,b);
 end
+toc();
